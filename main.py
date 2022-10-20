7bcc0cfb01c4d57487a33435ec7a198fbb5d77cb
Supharoek Kongprapane <pream5138@gmail.com>
2022-10-20 22:36:38 +0700

    add fake_db

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ async def root():
 async def read_item(item_id:str):
     return {"item_id": item_id}
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
