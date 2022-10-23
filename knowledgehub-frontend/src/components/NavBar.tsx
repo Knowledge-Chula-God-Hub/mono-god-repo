599dfd97f944a6fd44f1a3f7b576945fa865ab4e
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2022-10-23 16:56:06 +0700

    fix position

--- knowledgehub-frontend/src/components/NavBar.tsx
+++ knowledgehub-frontend/src/components/NavBar.tsx
@@ -8,6 +8,8 @@ function NavBar() {
                     justifyContent:"space-between",
                     padding:"0px 16px",
                     borderBottom: "2px solid black",
+                    position:"fixed",
+                    width:"99vw",
                     height:"80px"}}>
             <img src={BCulogo} width="83px"></img>
             <h1 style={{
