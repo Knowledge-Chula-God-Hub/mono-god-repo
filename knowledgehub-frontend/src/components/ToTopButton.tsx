a57c0d90a40f2225438bd0318455990c70b2489a
jirayuwat12 <49447426+jirayuwat12@users.noreply.github.com>
2022-10-24 12:28:38 +0700

    add scrollToTop button

---
+++ knowledgehub-frontend/src/components/ToTopButton.tsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {FaArrowCircleUp} from 'react-icons/fa';
+import styled from 'styled-components';
+import dropdown from '../assets/Dropdown.svg'
+const ScrollButton = () =>{
+
+const Button = styled.div`
+   position: fixed; 
+   width: 100%;
+   left: 96%;
+   bottom: 50px;
+   height: 20px;
+   font-size: 3rem;
+   z-index: 1;
+   cursor: pointer;
+   color: black;
+   
+`
+
+
+const [visible, setVisible] = useState(false)
+
+const toggleVisible = () => {
+	const scrolled = document.documentElement.scrollTop;
+	if (scrolled > 300){
+	setVisible(true)
+	}
+	else if (scrolled <= 300){
+	setVisible(false)
+	}
+};
+
+const scrollToTop = () =>{
+	window.scrollTo({
+	top: 0,
+	behavior: 'smooth'
+	});
+};
+
+window.addEventListener('scroll', toggleVisible);
+
+return (
+	<Button>
+	<img src={dropdown} onClick={scrollToTop}
+	style={{display: visible ? 'inline' : 'none',transform:"rotate(180deg)"}} />
+	</Button>
+);
+}
+
+export default ScrollButton;
