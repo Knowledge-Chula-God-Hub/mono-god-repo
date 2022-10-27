1e7705ff632652b0ef91a850dfedf167ccab234b
preamza02 <preamza02@gmail.com>
2022-10-27 16:42:36 +0700

    last commit

--- knowledgehub-frontend/src/pages/LoginPage.tsx
+++ knowledgehub-frontend/src/pages/LoginPage.tsx
@@ -12,18 +12,18 @@ function LoginPage() {
         if (ticket === '') window.location.href = "/"
         else {
             window.location.href = '/home'
-            getUserFromChulaSSO(ticket).then((data)=>{
-                console.log(data);
-                console.log(ticket);
-                console.log('http://localhost:8080'.concat("/serviceValidation".toString()))
-                // const NewUser:UserProps = {
-                //     id: Number(data.uid),
-                //     rank: 0,
-                //     name: data.firstname +" "+ data.lastname,
-                //     profileUrl: ""
-                // }
-                // user.setData?.(NewUser);
-            })
+            // getUserFromChulaSSO(ticket).then((data)=>{
+            //     console.log(data);
+            //     console.log(ticket);
+            //     console.log('http://localhost:8080'.concat("/serviceValidation".toString()))
+            //     const NewUser:UserProps = {
+            //         id: Number(data.uid),
+            //         rank: 0,
+            //         name: data.firstname +" "+ data.lastname,
+            //         profileUrl: ""
+            //     }
+            //     user.setData?.(NewUser);
+            // })
         }
 
     }
